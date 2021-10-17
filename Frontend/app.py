ef582b9c8f5c89c879af9d42effe17b7f94810b7
Jeffos1 <villa4ks@gmail.com>
2021-10-17 11:43:00 -0500

    jinja links y un navbar principal

--- Frontend/app.py
+++ Frontend/app.py
@@ -33,47 +33,47 @@ def dashboard():
 
 @app.route('/dashboard_child')
 def dashboard_child():
-    return render_template("dashboard_child.html")
+    return render_template("dashboard_child.html", pagina='dashboard')
 
 
 @app.route('/dashboard_vuelos')
-def dashboard_vuelo():
-    return render_template("dashboard_vuelos.html")
+def dashboard_vuelos():
+    return render_template("dashboard_vuelos.html", pagina='dashboard')
 
 
 @app.route('/dashboard_aviones')
 def dashboard_aviones():
-    return render_template("dashboard_aviones.html")
+    return render_template("dashboard_aviones.html", pagina='dashboard')
 
 
 @app.route('/dashboard_usuarios')
 def dashboard_usuarios():
-    return render_template("dashboard_usuarios.html")
+    return render_template("dashboard_usuarios.html", pagina='dashboard')
 
 
 @app.route('/dashboard_pilotos')
 def dashboard_pilotos():
-    return render_template("dashboard_pilotos.html")
+    return render_template("dashboard_pilotos.html", pagina='dashboard')
 
 
 @app.route('/usuario_piloto')
 def usuario_piloto():
-    return render_template("usuario_piloto.html")
+    return render_template("usuario_piloto.html", pagina='piloto')
 
 
 @app.route('/perfil_piloto')
 def perfil_piloto():
-    return render_template("perfil_piloto.html")
+    return render_template("perfil_piloto.html", pagina='piloto')
 
 
 @app.route('/mis_reservas')
 def mis_reservas():
-    return render_template("mis_reservas.html")
+    return render_template("mis_reservas.html", pagina='mis_reservas')
 
 
 @app.route('/dashboard_home')
 def dashboard_home():
-    return render_template('dashboard_home.html')
+    return render_template('dashboard_home.html', pagina='dashboard')
 
 
 @app.route('/opiniones')
@@ -98,7 +98,7 @@ def registrarse():
 
 @app.route('/reservar_tiquete', methods=['POST', 'GET'])
 def reservar_tiquete():
-    return render_template('reservar_tiquete.html')
+    return render_template('reservar_tiquete.html', pagina='usuario')
 
 
 if __name__ == '__main__':
