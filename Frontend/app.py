870a0d428c15efe8dbe061415ff882122c2b3e18
Jeffos1 <villa4ks@gmail.com>
2021-10-19 13:51:46 -0500

    en progreso

--- Frontend/app.py
+++ Frontend/app.py
@@ -119,8 +119,10 @@ def perfil_piloto():
     return render_template("perfil_piloto.html", pagina='piloto')
 
 
-@app.route('/mis_reservas')
+@app.route('/mis_reservas', methods)
 def mis_reservas():
+    if request.method == 'post':
+        print()
     user_id = session['id']
     query_origen_destino = f'SELECT  tiquetes.id, vuelos.c_salida, vuelos.c_destino, vuelos.salida, aviones.modelo, usuarios.nombres, usuarios.apellidos, tiquetes.precio, tiquetes.estado FROM tiquetes INNER JOIN vuelos ON tiquetes.vuelo=vuelos.id_vuelo INNER JOIN aviones on vuelos.avion = aviones.id_avion INNER JOIN usuarios on vuelos.piloto = usuarios.id WHERE tiquetes.usuario = {user_id}'
     tiquetes = seleccion(query_origen_destino)
